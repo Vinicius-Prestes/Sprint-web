40134cf665b7a1785d2210f9c84f6629a09dcf00
Vinicius Prestes <viniciusnevesprestes@gmail.com>
2024-06-16 17:30:29 -0300

    adicionando footer

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+//criação do carrossel
+let imagemIndex = 0;
+mostraimagem(imagemIndex);
+
+function mostraimagem(n) {
+    let imagem = document.getElementsByClassName("imagem-carrossel");
+    if (n >= imagem.length) { imagemIndex = 0 }
+    if (n < 0) { imagemIndex = imagem.length - 1 }
+    for (let i = 0; i < imagem.length; i++) {
+        imagem[i].style.display = "none";
+    }
+    imagem[imagemIndex].style.display = "block";
+}
+
+// função para mudar as imagens do carrossel
+function proxima() {
+    mostraimagem(++imagemIndex);
+}
+
+function anterior() {
+    mostraimagem(--imagemIndex);
+}
+
